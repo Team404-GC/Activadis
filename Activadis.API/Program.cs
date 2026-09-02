a2c237dd5b5f241bff47ccaf03ebeba2c9bfe0b7
DMWeggemans <daan.weggemans@student.graafschapcollege.nl>
2026-09-02 13:31:58 +0200

    initial

---
+++ Activadis.API/Program.cs
@@ -0,0 +1,46 @@
+namespace Activadis.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("api", new Microsoft.OpenApi.OpenApiInfo()
+                {
+                    Title = "API",
+                    Version = null
+                });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger(options =>
+                    options.RouteTemplate = "{documentName}/specifications.json");
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.RoutePrefix = "api";
+                    options.SwaggerEndpoint("specifications.json", "API");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
